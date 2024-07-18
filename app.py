16d35984cc665861f4a8d4bd9a556736228282b5
Amaan Al Mir <amaan.mir@pharmaciaty.com>
2024-07-18 12:14:29 +0400

    update

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# Importing modules from the 'src' folder
+# 'blog_app' is an FastAPI application
+# 'admin' is a Starlette application
 from src import blog_app, admin
 
 
@@ -5,6 +8,7 @@ app = blog_app()
 # app = admin()
 
 
+# Home/landing page
 @app.get("/")
 def home():
     return "Blog App - See APIs at /docs"
